ab8700b8d074b357b491b1708df48443da14e097
Alexander Scherer <alexscherer@outlook.com>
2019-12-04 13:12:55 +0100

    Add heroku config

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // TODO: Rewrite this file with dotenv https://www.npmjs.com/package/dotenv
 export default {
   POSTGRES_CONNECTION_STRING:
+    process.env.DATABASE_URL ||
     'postgres://ecommerce:ecommerce@localhost:5432/ecommerce'
 };
