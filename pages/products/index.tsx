dfeeba9a296411816f84a0d63a5eb49c8997d71a
Alexander Scherer <alexscherer@outlook.com>
2020-04-28 20:21:34 +0200

    Change to make Heroku work

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -88,7 +88,7 @@ const WithInitialProps: NextPage<Props> = ({ data }) => {
 
 WithInitialProps.getInitialProps = async ({ query }) => {
   const response = await fetch(
-    `https://` + process.env.HOST + `/api` || `http://localhost:3000/api`,
+    process.env.HOSTAPI || `http://localhost:3000/api`,
     {
       method: 'POST',
       headers: {
